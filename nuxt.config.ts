34006bea975865ec086dc21dcdfbbecae7489b50
Olga Kulczycka <olga@webjaksklep.eu>
2025-12-11 20:34:57 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/scss/main.scss'],
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
